6c24b992b6edec3b158eabaece800042fd216034
Katherine Kelli <katherinekelli.creates@gmail.com>
2026-04-17 10:29:41 -0500

    Initial Commit

---
+++ Friensy/FriensyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriensyApp.swift
+//  Friensy
+//
+//  Created by Katherine Pizzirusso on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct FriensyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

983a375447822cadc84f2c4b4673458573f52084
Maya Krishnan <mayark527@gmail.com>
2026-04-20 10:54:25 -0500

    Created Choices View

---
+++ Friensy/Choices.swift
@@ -0,0 +1,18 @@
+//
+//  Choices.swift
+//  Friensy
+//
+//  Created by Maya Krishnan on 4/20/26.
+//
+
+import SwiftUI
+
+struct Choices: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Choices()
+}
